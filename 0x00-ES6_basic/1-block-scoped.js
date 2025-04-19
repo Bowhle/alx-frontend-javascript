1ddec1d4d09f8d2a2b8e59e10daf85c4973888bd
bowhle <bowhlem@gmailcom>
2025-04-19 07:55:57 +0200

    1-block-scoped.js debugging unused vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const unusedTask = true;
+    const unusedTask2 = false;
   }
 
   return [task, task2];
