9f53fa29785d1d77b4bae808bda77bc766e27db7
bowhle <bowhlem@gmailcom>
2025-04-19 07:24:26 +0200

    Debugging 03

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,7 @@
-export default function returnHowManyArguments(...theArgs) {
-    return theArgs.length;
+export default function returnHowManyArguments(...args) {
+  let number = 0;
+  for (const _x of args) {
+    number += 1;
   }
+  return number;
+}
