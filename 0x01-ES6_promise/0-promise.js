461c853573a145d72a753646e56bd44f0d906f63
bowhle <bowhlem@gmailcom>
2025-04-20 20:55:47 +0200

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,6 @@
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // This simply returns a Promise that can be resolved or rejected later
+      resolve();
+    });
+  }
